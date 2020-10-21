3c72c21bc713fe7c5332dd4de6b4a37a8ad80a6f
AndreYMalo <72851945+AndreYMalo@users.noreply.github.com>
2020-10-21 09:19:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,72 @@
-#import telebot
-#bot = telebot.TeleBot("905266572:AAF0hAMoP01I2Vx-diR7_mgyhFw0NT4XWHI")
-# bot.send_message(1149221538, "Hello")
+import telebot
+import config
+import random
 
-# upd = bot.get_updates()
-# last_upd = upd[-1]
-# message_from_user = last_upd.message
-# print(message_from_user)
+from telebot import types
 
-#print(bot.get_me())
-#def log(message, answer):
-   # print('\n ------')
-   # from datetime import datetime
-    #print(datetime.now())
-    #print("Сообщение от {0} {1}. (id = {2}) \n Текст - {3}".format(message.from_user.first_name,
-                                                                   #message.from_user.last_name,
-                                                                   #str(message.from_user.id),
-                                                                   #message.text))
+bot = telebot.TeleBot("905266572:AAF0hAMoP01I2Vx-diR7_mgyhFw0NT4XWHI")
 
-#print(answear)
+#клавиатура
+@bot.message_handler(commands=["start"])
+def handle_command(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, "True/False")
+    user_markup.row("🎲 Рандомное число","😊 Как дела?")
 
-#@bot.message_handler(commands=["start"])
-#def handler_start(message):
-     user_markup = telebot.types.ReplyKeyboardMarkup()
-     user_markup.row("/start","/stop")
-     user_markup.row("fsdfag", "photo")
-     user_markup.row("music", "video")
-     bot.send_message(message.from_user.id, "Выбирай!",reply_markup=user_markup)
-#
-#
-#
-#
-# #    print("ПРИШЛО СООБЩЕНИЕ ОМАГАД")
-bot.polling(none_stop=True, interval=0)
+
+    @bot.message_handler(commands=['start'])
+    def welcome(message):
+        sti = open('static/welcome.webp', 'rb')
+        bot.send_sticker(message.chat.id, sti)
+    # keyboard
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("🎲 Рандомное число")
+    item2 = types.KeyboardButton("😊 Как дела?")
+
+    markup.add(item1, item2)
+
+    bot.send_message(message.chat.id,
+                     "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы быть подопытным кроликом.".format(
+                         message.from_user, bot.get_me()),
+                     parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+    if message.chat.type == 'private':
+        if message.text == '🎲 Рандомное число':
+            bot.send_message(message.chat.id, str(random.randint(0, 100)))
+        elif message.text == '😊 Как дела?':
+
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
+            item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
+
+            markup.add(item1, item2)
+
+            bot.send_message(message.chat.id, 'Отлично, сам как?', reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, 'Я не знаю что ответить 😢')
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'good':
+                bot.send_message(call.message.chat.id, 'Вот и отличненько 😊')
+            elif call.data == 'bad':
+                bot.send_message(call.message.chat.id, 'Бывает 😢')
+
+            # remove inline buttons
+
+
+            # show alert
+            bot.answer_callback_query(callback_query_id=call.id, show_alert=False,
+                                      text="ЭТО ТЕСТОВОЕ УВЕДОМЛЕНИЕ!!11")
+
+    except Exception as e:
+        print(repr(e))
+
+
+# RUN
+bot.polling(none_stop=True)
