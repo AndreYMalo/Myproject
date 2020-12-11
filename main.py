c409f0dde39abb148c4b3aedeb6879ff3e62d0ca
AndreYMalo <72851945+AndreYMalo@users.noreply.github.com>
2020-12-11 09:37:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import telebot
 covid19 = COVID19Py.COVID19()
 bot = telebot.TeleBot('1453562215:AAEzET3P8qhTpWM9E7Zz-cFFq-SAKBzCXUs')
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
@@ -17,11 +18,14 @@ def start(message):
     item8 = types.KeyboardButton('Германия')
     item9 = types.KeyboardButton('Япония')
     item10 = types.KeyboardButton('США')
-    markup.add(item1, item2, item3, item4, item5, item7, item8, item9, item10)
+    item11 = types.KeyboardButton('Турция')
+    markup.add(item1, item2, item3, item4, item5, item7, item8, item9, item10, item11)
 
     send_message = f"<b>Пиривет, {message.from_user.first_name}!</b>\nХочешь узнать статистику по короне, тогда напиши " \
                    f"название страны, например: США, Украина, Россия и так далее."
+
     bot.send_message(message.chat.id, send_message, parse_mode='html', reply_markup=markup)
+
 
 @bot.message_handler(content_types=['text'])
 def mess(message):
@@ -29,55 +33,53 @@ def mess(message):
     get_message_bot = message.text.strip().lower()
     if get_message_bot == "сша":
         location = covid19.getLocationByCountryCode("US")
-        sti = open('Flagi/США.jpg', 'rb')
-        bot.send_sticker(message.chat.id, sti)
+        photo = open('C:\Flagi/США.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
     elif get_message_bot == "украина":
         location = covid19.getLocationByCountryCode("UA")
-        sti = open('Flagi/Украина.jpg', 'rb')
-        bot.send_sticker(message.chat.id, sti)
+        photo = open('C:\Flagi/Украина.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
     elif get_message_bot == "россия":
         location = covid19.getLocationByCountryCode("RU")
-        sti = open('Flagi/Россия.jpg', 'rb')
-        bot.send_sticker(message.chat.id, sti)
+        photo = open('C:\Flagi/Россия.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
     elif get_message_bot == "беларусь":
         location = covid19.getLocationByCountryCode("BY")
-        sti = open('Flagi/Беларусь.jpg', 'rb')
-        bot.send_sticker(message.chat.id, sti)
+        photo = open('C:\Flagi/www.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
     elif get_message_bot == "италия":
         location = covid19.getLocationByCountryCode("IT")
-        sti = open('Flagi/Италия.jpg', 'rb')
-        bot.send_sticker(message.chat.id, sti)
+        photo = open('C:\Flagi/Италия.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
     elif get_message_bot == "франция":
         location = covid19.getLocationByCountryCode("FR")
-        sti = open('Flagi/Франция.jpg', 'rb')
-        bot.send_sticker(message.chat.id, sti)
+        photo = open('C:\Flagi/Франция.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
     elif get_message_bot == "германия":
         location = covid19.getLocationByCountryCode("DE")
-        sti = open('Flagi/Германия.jpg', 'rb')
-        bot.send_sticker(message.chat.id, sti)
+        photo = open('C:\Flagi/Германия.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
     elif get_message_bot == "япония":
         location = covid19.getLocationByCountryCode("JP")
-        sti = open('Flagi/Япония.jpg', 'rb')
-        bot.send_sticker(message.chat.id, sti)
+        photo = open('C:\Flagi/Япония.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
     elif get_message_bot == "турция":
         location = covid19.getLocationByCountryCode("TR")
-        sti = open('Flagi/Турция.jpg', 'rb')
-        bot.send_sticker(message.chat.id, sti)
+        photo = open('C:\Flagi/Турция.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
     else:
         location = covid19.getLatest()
-        final_message = f"<u>Данные по всему миру:</u>\n<b>Заболевших: </b>{location['confirmed']:,}\n<b>Сметрей: </b>{location['deaths']:,}\n<b>Выздоровели: </b>" \
-                        f"{location[0]['latest']['recovered']:,}"
+        final_message = f"<u>Данные по всему миру:</u>\n<b>Заразились короной: </b>{location['confirmed']:,}\n<b>Погибло: </b>{location['deaths']:,}"
 
 
     if final_message == "":
-
         date = location[0]['last_updated'].split("T")
         time = date[1].split(".")
-        final_message = f"<u>Данные по стране:</u>\nНаселение: {location[0]['country_population']:,}\n" \
-                        f"Последнее обновление: {date[0]} {time[0]}\nПоследние данные:\n<b>" \
-                        f"Зар: </b>{location[0]['latest']['confirmed']:,}\n<b>Смертей: </b>" \
-                        f"{location[0]['latest']['deaths']:,}\n<b>Выздоровели: </b>" \
-                        f"{location[0]['latest']['recovered']:,}"
+        final_message = f"<u>Данные по стране:</u>\nЖивёт в стране: {location[0]['country_population']:,}\n" \
+                        f"Последнее обновление: {date[0]} {time[0]}\nПоследние новости:\n<b>" \
+                        f"Заразились короной: </b>{location[0]['latest']['confirmed']:,}\n<b>Погибло: </b>" \
+                        f"{location[0]['latest']['deaths']:,}"
+
 
     bot.send_message(message.chat.id, final_message, parse_mode='html')
 
